f0adab79c96e4949ebbe2e11cc8423ce29360574
Matthew Campbell <matthewcampbell@pal-nat186-132-191.itap.purdue.edu>
2023-02-04 17:38:54 -0500

    Scoring

--- ratom/metrics/correctness.go
+++ ratom/metrics/correctness.go
@@ -1,5 +1,9 @@
 package metrics
 
+import (
+)
+
 func Correctness() float32 {
+
 	return 0;
 }
--- ratom/score.go
+++ ratom/score.go
@@ -3,6 +3,9 @@ package ratom
 import (
 	"github.com/paingp/ece461-project-cli/ratom/metrics"
 	"net/http"
+	"fmt"
+	"encoding/json"
+	//"io/ioutil"
 )
 
 type Module struct {
@@ -15,7 +18,40 @@ type Module struct {
 	license     bool
 }
 
+func formatUrl(url string) string {
+	return "";
+}
+
 func Analyze(url string, client *http.Client) Module {
+	resp, error := client.Get("https://api.github.com/repos/lencx/ChatGPT")
+
+	if error != nil {
+		panic(error)
+	}
+
+	defer resp.Body.Close()
+
+	var j interface{}
+	error = json.NewDecoder(resp.Body).Decode(&j)
+	if error != nil {
+			panic(error)
+	}
+	fmt.Printf("%s", j)
+	fmt.Println(resp)
+
+	// defer resp.Body.Close()
+
+	// body, error := ioutil.ReadAll(resp.Body)
+	// if error != nil {
+	// 	panic(error)
+	// }
+
+	// fmt.Println(string(body))
+
+	if error != nil {
+		panic(error)
+	}
+
 	c := metrics.Correctness()
 	m := Module{url, -1, -1, c, -1, -1, false}
 	return m
