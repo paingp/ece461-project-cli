ed25b14e3cc80e338a6caedc2e243acf68510674
AdityaSri11 <srikant2@purdue.edu>
2023-02-08 21:49:31 -0500

    Finished logging

---
+++ ratom/metrics/metrics_test.go
@@ -0,0 +1,50 @@
+package metrics
+
+import (	
+	"context"
+	"os"
+	"testing"
+	"net/http"
+	"io/ioutil"
+	"encoding/json"
+
+
+	"golang.org/x/oauth2"
+)
+
+var endpoint = "https://api.github.com/repos/cloudinary/cloudinary_npm"
+
+func TestBusFactor(t *testing.T) {
+	src := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: os.Getenv("GITHUB_TOKEN")},
+	)
+	httpClient := oauth2.NewClient(context.Background(), src)
+
+	resp, error := httpClient.Get(endpoint)
+
+	if error != nil {
+		panic(error)
+	}
+
+	if resp.StatusCode == http.StatusOK {
+		bodyBytes, err := ioutil.ReadAll(resp.Body)
+
+		if err != nil {
+			panic(error)
+		}
+		bodyString := string(bodyBytes)
+
+		// Citation needed for this
+		resBytes := []byte(bodyString)
+		var jsonRes map[string]interface{}
+		_ = json.Unmarshal(resBytes, &jsonRes)
+
+		var bus = BusFactor(jsonRes)
+
+		if bus < 0 || bus > 1 {
+			t.Fatalf("Bus is out of range")
+		}
+	}
+
+	defer resp.Body.Close()
+}
