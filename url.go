e495220c7333f8765695b7ae72e53f56d351d80c
Brian Ng <ng118@eceprog2.ecn.purdue.edu>
2023-02-08 22:28:22 -0500

    Url Testing

--- url.go
+++ url.go
@@ -3,23 +3,27 @@ package main
 import (
     "net/http"
 	"strings"
+	"fmt"
+
+	"github.com/paingp/ece461-project-cli/ratom"
     "github.com/gin-gonic/gin"
+
 )
 
 // album represents data about a record album.
-type album struct {
-    ID     string  `json:"id"`
-    Title  string  `json:"title"`
-    Artist string  `json:"artist"`
-    Price  float64 `json:"price"`
-}
+//type album struct {
+//    ID     string  `json:"id"`
+//    Title  string  `json:"title"`
+//    Artist string  `json:"artist"`
+//    Price  float64 `json:"price"`
+//}
 
 // albums slice to seed record album data.
-var albums = []album{
-    {ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-    {ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-    {ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
-}
+//var albums = []album{
+//    {ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+//    {ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+//    {ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+//}
 
 func main() {
     //router := gin.Default()
@@ -84,6 +88,51 @@ func main() {
 
 	
 }
+func getEndpoint(url string) string {
+    endpoint := "Invalid"
+    //if (strings.Contains(url, "github"))
+    before, after, found := strings.Cut(url, "www")
+    if found {
+        endpoint = before + "registry" + after
+        endpoint = strings.Replace(endpoint, "com", "org", 1)
+        endpoint = strings.Replace(endpoint, "package/", "", 1)
+
+        resp, err := http.Get(endpoint)
+
+        if err != nil {
+            return "Invalid"
+        }
+
+        if resp.StatusCode == http.StatusOK {
+            bodyBytes, err := ioutil.ReadAll(resp.Body)
+
+            if err != nil {
+                panic(err)
+            }
+            bodyString := string(bodyBytes)
+
+            resBytes := []byte(bodyString)
+            var npmRes map[string]interface{}
+            _ = json.Unmarshal(resBytes, &npmRes)
+
+            bugs := npmRes["bugs"].(map[string]interface{})
+            endpoint = bugs["url"].(string)
+            endpoint = endpoint[:8] + "api." + strings.Replace(endpoint[8:], "/", "/repos/", 1)
+            endpoint = endpoint[:len(endpoint)-7]
+        }
+
+    } else {
+        index := strings.Index(url, "github")
+        if index != -1 {
+            //fmt.Print(url[index:])
+            endpoint = "https://api/." + strings.Replace(url[index:], "/", "/repos/", 1)
+        }
+        //endpoint = subStrings[0] + "api." + subStrings[1]
+    }
+    return endpoint
+}
+//Sample Code from testing
+//
 // getAlbums responds with the list of all albums as JSON.
 //func getAlbums(c *gin.Context) {
 //   c.IndentedJSON(http.StatusOK, albums)
