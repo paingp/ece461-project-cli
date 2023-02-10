5da3187c9db7172fecdba06bdc251ccd45aba338
AdityaSri11 <srikant2@purdue.edu>
2023-02-10 13:25:12 -0500

    First GraphQL mettic

--- ratom/score.go
+++ ratom/score.go
@@ -3,7 +3,6 @@ package ratom
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -126,18 +125,6 @@ func Analyze(url string, client *http.Client) Module {
 	var license bool
 	var netScore float32
 
-	var Data struct {
-		Viewer struct {
-			Login string
-		}
-		Repository struct {
-			CommitComments struct {
-				TotalCount int
-			}
-		} `graphql:"repository(owner: \"expressjs\", name: \"express\")"`
-	}
-
-
 	oldURL := url
 	url = getEndpoint((url))
 	lineNumb := metrics.File_line()
@@ -148,14 +135,8 @@ func Analyze(url string, client *http.Client) Module {
 	if error != nil {
 		panic(error)
 	}
-
-	graphQLClient := githubv4.NewClient(client)
-	error = graphQLClient.Query(context.Background(), &Data, nil)
-	if error != nil {
-		panic(error)
-	}
-
-	fmt.Println(Data.Repository.CommitComments.TotalCount)
+	
+	//fmt.Println(Data.Repository.CommitComments.TotalCount)
 
 	if resp.StatusCode == http.StatusOK {
 		bodyBytes, err := ioutil.ReadAll(resp.Body)
@@ -170,6 +151,31 @@ func Analyze(url string, client *http.Client) Module {
 		var jsonRes map[string]interface{}
 		_ = json.Unmarshal(resBytes, &jsonRes)
 
+		// GRAPH QL 
+		owner_map := jsonRes["owner"].(map[string]interface{})
+
+		var Data struct {
+			Viewer struct {
+				Login string
+			}
+			Repository struct {
+				CommitComments struct {
+					TotalCount int
+				}
+			} `graphql:"repository(owner: $owner, name: $name)"`
+		}
+	
+		variables := map[string]interface{}{
+			"owner": githubv4.String(owner_map["login"].(string)),
+			"name":  githubv4.String(jsonRes["name"].(string)),
+		}
+	
+		graphQLClient := githubv4.NewClient(client)
+		error = graphQLClient.Query(context.Background(), &Data, variables)
+		if error != nil {
+			panic(error)
+		}
+		
 		//name := jsonRes["id"].(float64)
 		correctnessScore = metrics.Correctness(jsonRes)
 		lineNumb := metrics.File_line()
@@ -179,7 +185,7 @@ func Analyze(url string, client *http.Client) Module {
 		lineNumb = metrics.File_line()
 		metrics.Functions = append(metrics.Functions, "Function: metrics.BusFactor called on score.go at line "+lineNumb)
 
-		rampUp = metrics.RampUp(jsonRes)
+		rampUp = metrics.RampUp(jsonRes, Data.Repository.CommitComments.TotalCount)
 		lineNumb = metrics.File_line()
 		metrics.Functions = append(metrics.Functions, "Function: metrics.RampUp called on score.go at line "+lineNumb)
 
