541f95239a6cfadbb61dec2379c894c44ec75b69
Paing Hein Khant <pkhant@eceprog6.ecn.purdue.edu>
2023-02-11 12:36:48 -0500

    Implementing regex for Readme, Refactor

--- ratom/score.go
+++ ratom/score.go
@@ -1,12 +1,15 @@
 package ratom
 
 import (
+	// "bufio"
+	// "bytes"
+	//"fmt"
 	"encoding/json"
-	"io/ioutil"
+	"io"
 	"log"
 	"net/http"
 	"os"
-	//"os/exec"
+	//"regexp"
 	"strings"
 
 	"github.com/paingp/ece461-project-cli/ratom/metrics"
@@ -25,23 +28,21 @@ type Module struct {
 	License     bool
 }
 
-func getEndpoint(url string) string {
-	endpoint := "Invalid"
-	//if (strings.Contains(url, "github"))
+func getGithubUrl(url string) string {
 	before, after, found := strings.Cut(url, "www")
 	if found {
-		endpoint = before + "registry" + after
-		endpoint = strings.Replace(endpoint, "com", "org", 1)
-		endpoint = strings.Replace(endpoint, "package/", "", 1)
+		npmEndpoint := before + "registry" + after
+		npmEndpoint = strings.Replace(npmEndpoint, "com", "org", 1)
+		npmEndpoint = strings.Replace(npmEndpoint, "package/", "", 1)
 
-		resp, err := http.Get(endpoint)
+		resp, err := http.Get(npmEndpoint)
 
 		if err != nil {
 			return "Invalid"
 		}
 
 		if resp.StatusCode == http.StatusOK {
-			bodyBytes, err := ioutil.ReadAll(resp.Body)
+			bodyBytes, err := io.ReadAll(resp.Body)
 
 			if err != nil {
 				panic(err)
@@ -53,87 +54,83 @@ func getEndpoint(url string) string {
 			_ = json.Unmarshal(resBytes, &npmRes)
 
 			bugs := npmRes["bugs"].(map[string]interface{})
-			endpoint = bugs["url"].(string)
-			Clone(strings.Replace(endpoint, "/issues", ".git", 1))
-			endpoint = endpoint[:8] + "api." + strings.Replace(endpoint[8:], "/", "/repos/", 1)
-			endpoint = endpoint[:len(endpoint)-7]
+			npmEndpoint = bugs["url"].(string)
+			url = strings.Replace(npmEndpoint, "/issues", "", 1)
 		}
-
-	} else {
-		Clone(url + ".git")
-		index := strings.Index(url, "github")
-		if index != -1 {
-			//fmt.Print(url[index:])
-			endpoint = "https://api." + strings.Replace(url[index:], "/", "/repos/", 1)
-		}
-		//endpoint = subStrings[0] + "api." + subStrings[1]
 	}
-	return endpoint
+	return url
+}
+
+func getEndpoint(url string) string {
+	index := strings.Index(url, "github")
+	url = "https://api." + strings.Replace(url[index:], "/", "/repos/", 1)
+	return url
 }
 
 func GetToken() string {
 	return os.Getenv("GITHUB_TOKEN")
 }
 
-func Clone(repo string) {
+func Clone(repo string) string {
 
 	// Temp directory to clone the repository
 	if GITHUB_TOKEN == "" {
 		GITHUB_TOKEN = GetToken()
 	}
 
-	dir, err := os.MkdirTemp("temp", "repo")
-	//fmt.Println(url)
+	lastIdx := strings.LastIndex(repo, "/")
+	//fmt.Println(repo[lastIdx + 1:])
+	dir := "temp/" + repo[lastIdx + 1:]
+
+	err := os.MkdirAll(dir, 0777)
 
 	if err != nil {
 		panic(err)
+		return ""
 	}
 
-	defer os.RemoveAll(dir)
-	log.Println(dir)
-	log.Println(repo)
+	//defer os.RemoveAll(dir)
 
 	_, err = git.PlainClone(dir, false, &git.CloneOptions{
-		URL: repo,
+		URL: repo + ".git",
 		SingleBranch: true,
 		Depth: 1,
 	})
 		
 	if err != nil {
 		log.Fatal(err)
+		return "err"
 	}
-
-	// cmd := exec.Command("ls | grep -i readme", dir)
-	// out, err := cmd.Output()
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// log.Println(string(out))
+	return dir
 }
 
 func Analyze(url string, client *http.Client) Module {
 
-	var correctnessScore float32
 	var busFactor float32
-	var rampUp float32
 	var responsiveMaintainer float32
+	var correctnessScore float32
+	var rampUp float32
 	var license bool
 	var netScore float32
 
-	oldURL := url
-	url = getEndpoint((url))
+	//oldURL := url
+
+	gitUrl := getGithubUrl(url)
+	//fmt.Println(gitUrl)
+	dir := Clone(gitUrl)
+
+	endpoint := getEndpoint(gitUrl)
 	lineNumb := metrics.File_line()
 	metrics.Functions = append(metrics.Functions, "Function: getEndpoint called on score.go at line " + lineNumb)
 
-	resp, error := client.Get(url)
+	resp, error := client.Get(endpoint)
 
 	if error != nil {
 		panic(error)
 	}
 
 	if resp.StatusCode == http.StatusOK {
-		bodyBytes, err := ioutil.ReadAll(resp.Body)
+		bodyBytes, err := io.ReadAll(resp.Body)
 
 		if err != nil {
 			panic(error)
@@ -162,7 +159,8 @@ func Analyze(url string, client *http.Client) Module {
 		lineNumb = metrics.File_line()
 		metrics.Functions = append(metrics.Functions, "Function: metrics.ResponsiveMaintainer called on score.go at line " + lineNumb)
 
-		license = metrics.License(jsonRes)
+
+		license = metrics.License(dir)
 		lineNumb = metrics.File_line()
 		metrics.Functions = append(metrics.Functions, "Function: metrics.License called on score.go at line " + lineNumb)
 
@@ -171,9 +169,8 @@ func Analyze(url string, client *http.Client) Module {
 		metrics.Functions = append(metrics.Functions, "Function: metrics.NetScore called on score.go at line " + lineNumb)
 	}
 
-	m := Module{oldURL, netScore, rampUp, correctnessScore, busFactor, responsiveMaintainer, license}
 	defer resp.Body.Close()
+
+	m := Module{url, netScore, rampUp, correctnessScore, busFactor, responsiveMaintainer, license}
 	return m
 }
-
-//func Sort([] ratom.Module)
