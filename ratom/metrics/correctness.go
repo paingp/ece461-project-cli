518e2f0c51f0d21d0ab4d02ee77886bd9d4ef2f8
Matthew <campb474@purdue.edu>
2023-02-11 13:39:05 -0500

    Finished testing and coverage

--- ratom/metrics/correctness.go
+++ ratom/metrics/correctness.go
@@ -29,7 +29,9 @@ func Correctness(jsonRes map[string]interface{}) float32 {
 	}
 
 	// Assigning weights to stargazers
-	stargazers = math.Min(1, math.Log10(stargazers)/5.5)
+	stargazers = math.Log10(stargazers) // Finding log of stargazers
+	stargazers = stargazers / 5.5 // Normalizaing to 100,000
+	stargazers = math.Min(1, stargazers) // Capping stargazers at 1
 	// if 0 < stargazers && stargazers <= 50 {
 	// 	stargazers = .05
 	// } else if stargazers <= 200 {
@@ -42,7 +44,9 @@ func Correctness(jsonRes map[string]interface{}) float32 {
 	// 	stargazers = .3
 	// }
 
-	forksNum = math.Min(1, math.Log10(forksNum)/5.5)
+	forksNum = math.Log10(forksNum) 
+	forksNum = forksNum / 5.5
+	forksNum = math.Min(1, forksNum)
 	// if forksNum <= 50 {
 	// 	forksNum = .05
 	// } else if forksNum <= 200 {
@@ -55,7 +59,9 @@ func Correctness(jsonRes map[string]interface{}) float32 {
 	// 	forksNum = .3
 	// }
 
-	subscriberCount = math.Min(1, math.Log10(subscriberCount)/5.5)
+	subscriberCount = math.Log10(subscriberCount)
+	subscriberCount = subscriberCount / 5.5
+	subscriberCount = math.Min(1, subscriberCount)
 	// if subscriberCount <= 50 {
 	// 	subscriberCount = .02
 	// } else if subscriberCount <= 200 {
